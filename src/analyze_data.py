5533c652eed8478c08482dd5456a65822ab2c3ab
Alexander Guziak <guziakalex@gmail.com>
2022-04-02 17:37:23 -0500

    Aggregating results for the entire year

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,6 +1,7 @@
+import fastf1
+
 import src.get_data
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -26,21 +27,45 @@ def create_optimal_lap_times(lap_timing_data: pd.DataFrame) -> pd.DataFrame:
     return optimal_lap_times
 
 
-if __name__ == '__main__':
-    fp1_session_data_df = src.get_data.get_timing_data_for_session(
-        session_year=2021,
-        session_round=1,
-        session_identifier='FP1')
-    fp2_session_data_df = src.get_data.get_timing_data_for_session(
-        session_year=2021,
-        session_round=1,
-        session_identifier='FP2')
-    fp3_session_data_df = src.get_data.get_timing_data_for_session(
-        session_year=2021,
-        session_round=1,
-        session_identifier='FP3')
+def get_time_differences_for_race_weekend(session_year: int, session_round: int) -> pd.DataFrame:
+    event = src.get_data.get_event_data_for_session(session_year, session_round)
+    is_sprint_race_weekend = event.get_session_name(3) != 'Practice 3'
 
-    full_testing_data_df = pd.concat([fp1_session_data_df, fp2_session_data_df, fp3_session_data_df], axis=0)
+    retrieved_session_data = []
+
+    try:
+        fp1_session_data_df = src.get_data.get_timing_data_for_session(
+            session_year=session_year,
+            session_round=session_round,
+            session_identifier='FP1')
+        retrieved_session_data.append(fp1_session_data_df)
+    except fastf1.core.DataNotLoadedError:
+        print(f'No data for event: Year {session_year} round {session_round} FP1')
+
+    if not is_sprint_race_weekend:
+        """
+            There is a second free practice session on sprint race weekends, however this occurs after the traditional
+                qualifying process used for the sprint race and will not be considered
+        """
+        try:
+            fp2_session_data_df = src.get_data.get_timing_data_for_session(
+                session_year=session_year,
+                session_round=session_round,
+                session_identifier='FP2')
+            retrieved_session_data.append(fp2_session_data_df)
+        except fastf1.core.DataNotLoadedError:
+            print(f'No data for event: Year {session_year} round {session_round} FP1')
+
+        try:
+            fp3_session_data_df = src.get_data.get_timing_data_for_session(
+                session_year=session_year,
+                session_round=session_round,
+                session_identifier='FP3')
+            retrieved_session_data.append(fp3_session_data_df)
+        except fastf1.core.DataNotLoadedError:
+            print(f'No data for event: Year {session_year} round {session_round} FP1')
+
+    full_testing_data_df = pd.concat(retrieved_session_data, axis=0)
 
     optimal_lap_times_df = create_optimal_lap_times(full_testing_data_df) \
         .sort_values(by='TotalLapTime') \
@@ -58,13 +83,12 @@ if __name__ == '__main__':
         .rename(columns={'TotalLapTime': 'OptimalFPLapTime'})
 
     qualifying_results_df = src.get_data.get_results_for_session(
-        session_year=2021,
-        session_round=1,
+        session_year=session_year,
+        session_round=session_round,
         session_identifier='Q'
     )
 
     qualifying_results_df = qualifying_results_df[['DriverNumber', 'Q1', 'Q2', 'Q3']]
-
 
     def select_qualifying_time(row: pd.Series) -> pd.Series:
         """
@@ -85,7 +109,6 @@ if __name__ == '__main__':
             return pd.Series({'QualifyingTime': row['Q2']})
         else:
             return pd.Series({'QualifyingTime': row['Q1']})
-
 
     qualifying_times = qualifying_results_df \
         .apply(select_qualifying_time, axis=1)
@@ -109,18 +132,33 @@ if __name__ == '__main__':
     time_difference_df['AbsErrorQOpt'] = time_difference_df['TimeDifferenceSecondsQOpt'].apply(abs)
     time_difference_df['AbsErrorQSingle'] = time_difference_df['TimeDifferenceSecondsQSingle'].apply(abs)
 
+    return time_difference_df
+
+
+if __name__ == '__main__':
+    year = 2021
+
+    event_schedule = src.get_data.get_event_schedule_for_year(year)
+    df = pd.DataFrame()
+
+    for round_num in event_schedule['RoundNumber'].tolist():
+        print(f'Processing round {round_num}')
+        df = pd.concat([df, get_time_differences_for_race_weekend(year, round_num)], axis=0)
+
     fig, (ax1, ax2) = plt.subplots(2, 1)
 
     fig: plt.Figure
     ax1: plt.Axes
     ax2: plt.Axes
 
-    ax1.hist(x=time_difference_df['TimeDifferenceSecondsQSingle'], edgecolor='k', linewidth=1)
+    n_bins = 100
+
+    ax1.hist(x=df['TimeDifferenceSecondsQSingle'], edgecolor='k', linewidth=1, bins=n_bins)
     ax1.set_title('Error Dist. Whole Fastest Lap')
     ax1.set_xlabel('Time Difference (sec)')
     ax1.set_ylabel('Count')
 
-    ax2.hist(x=time_difference_df['TimeDifferenceSecondsQOpt'], edgecolor='k', linewidth=1)
+    ax2.hist(x=df['TimeDifferenceSecondsQOpt'], edgecolor='k', linewidth=1, bins=n_bins)
     ax2.set_title('Error Dist. Only Fastest Sectors')
     ax2.set_xlabel('Time Difference (sec)')
     ax2.set_ylabel('Count')
--- src/get_data.py
+++ src/get_data.py
@@ -24,11 +24,51 @@ def get_timing_data_for_session(
     cache_path.mkdir(parents=True, exist_ok=True)
 
     fastf1.Cache.enable_cache(str(cache_path))
+
     session = fastf1.get_session(session_year, session_round, session_identifier)
     session.load(telemetry=False)
     accurate_laps = session.laps.pick_accurate()
     df = pd.DataFrame(accurate_laps)
     return df
+
+
+def get_event_data_for_session(session_year: int, session_round: int):
+    """
+    Retrieves the event data for a given session
+
+    Args:
+        session_year (int): The year in which the session takes place
+        session_round (int): The round of the session
+
+    Returns:
+        Event: fastf1 Event object
+
+    """
+    cache_path = pathlib.Path('../fastf1_cache')
+    cache_path.mkdir(parents=True, exist_ok=True)
+
+    fastf1.Cache.enable_cache(str(cache_path))
+
+    return fastf1.get_event(session_year, session_round)
+
+
+def get_event_schedule_for_year(year: int):
+    """
+    Gets the event schedule for an entire year, excluding testing
+
+    Args:
+        year (int): The four-digit year
+
+    Returns:
+        EventSchedule: fastf1 EventSchedule object
+
+    """
+    cache_path = pathlib.Path('../fastf1_cache')
+    cache_path.mkdir(parents=True, exist_ok=True)
+
+    fastf1.Cache.enable_cache(str(cache_path))
+
+    return fastf1.get_event_schedule(year, include_testing=False)
 
 
 def get_results_for_session(
@@ -51,6 +91,7 @@ def get_results_for_session(
     cache_path.mkdir(parents=True, exist_ok=True)
 
     fastf1.Cache.enable_cache(str(cache_path))
+
     session = fastf1.get_session(session_year, session_round, session_identifier)
     session.load(telemetry=False)
     df = pd.DataFrame(session.results)
