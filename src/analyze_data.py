81815537376e4ece12828ce097627237e8b46abf
Alexander Guziak <guziakalex@gmail.com>
2022-04-22 17:44:12 -0500

    Plotting the score distribution

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -258,7 +258,7 @@ def score_linear_regression_model(df: pd.DataFrame, num_k_folds=5) -> float:
     df['PctTimeDifferenceSecondsQSingle'] = (df['TimeDifferenceSecondsQSingle'] / df['QualifyingTimeSeconds']) * 100.
     df['PctTimeDifferenceSecondsQOpt'] = (df['TimeDifferenceSecondsQOpt'] / df['QualifyingTimeSeconds']) * 100.
 
-    scores = list()
+    k_fold_scores = list()
 
     group_k_fold = GroupShuffleSplit(n_splits=num_k_folds)
 
@@ -283,9 +283,9 @@ def score_linear_regression_model(df: pd.DataFrame, num_k_folds=5) -> float:
                                .groupby(by='Round')
                                .apply(spearman_rho))
 
-        scores.append(k_fold_score)
+        k_fold_scores.append(k_fold_score)
 
-    avg_score = np.average(scores)
+    avg_score = np.average(k_fold_scores)
     print(f'Score: {avg_score}')
     return avg_score
 
@@ -307,12 +307,14 @@ def get_timing_data(years: List[int]) -> pd.DataFrame:
     return df
 
 
-def plot_error_dist(errors: pd.Series):
+def plot_error_dist(errors: pd.Series, plot_z_score: bool = False, error_name: str = 'Error'):
     """
     Creates a histogram and QQ plot for the provided error distribution
 
     Args:
         errors (Series): Pandas Series object containing error data
+        error_name (str): The name of the error being plotting, which will be used for axis labeling
+        plot_z_score (bool): If true, will create the histogram using z-scores instead of raw scores
 
     """
 
@@ -328,16 +330,24 @@ def plot_error_dist(errors: pd.Series):
     avg = np.average(errors)
     z_scores = (errors - avg) / std_dev
 
-    bin_width = (z_scores.max() - z_scores.min()) / n_bins
-    gaussian_x = np.linspace(min(z_scores), max(z_scores), 100)
-    gaussian_y = scipy.stats.norm.pdf(gaussian_x, 0, 1)
-    gaussian_y *= (len(errors) * bin_width)
+    if plot_z_score:
+        bin_width = (z_scores.max() - z_scores.min()) / n_bins
+        gaussian_x = np.linspace(min(z_scores), max(z_scores), 100)
+        gaussian_y = scipy.stats.norm.pdf(gaussian_x, 0, 1)
+        gaussian_y *= (len(errors) * bin_width)
+        ax1.hist(x=z_scores, edgecolor='k', linewidth=1, bins=n_bins)
+        ax1.set_xlabel('Z-Score')
+    else:
+        bin_width = (errors.max() - errors.min()) / n_bins
+        gaussian_x = np.linspace(min(errors), max(errors), 100)
+        gaussian_y = scipy.stats.norm.pdf(gaussian_x, avg, std_dev)
+        gaussian_y *= (len(errors) * bin_width)
+        ax1.hist(x=errors, edgecolor='k', linewidth=1, bins=n_bins)
+        ax1.axvline(x=avg, label=f'Mean Value ({avg:.2f})', color='k', linestyle='--')
+        ax1.set_xlabel(f'{error_name}')
 
-    ax1.hist(x=z_scores, edgecolor='k', linewidth=1, bins=n_bins)
     ax1.plot(gaussian_x, gaussian_y, color='r', linestyle='--', label='Scaled Normal Curve')
-
-    ax1.set_title('Individual Pct Differences Error Distribution')
-    ax1.set_xlabel('Z-Score')
+    ax1.set_title('Error Distribution')
     ax1.set_ylabel('Count')
     ax1.legend()
 
@@ -358,5 +368,6 @@ def plot_error_dist(errors: pd.Series):
 
 if __name__ == '__main__':
     timing_df = get_timing_data([2021])
-    scores = [score_linear_regression_model(timing_df, num_k_folds=5) for i in range(100)]
+    scores = [score_linear_regression_model(timing_df, num_k_folds=5) for i in range(500)]
+    plot_error_dist(pd.Series(scores), plot_z_score=False, error_name="Spearman's Rho")
     print(scores)
